749908e21dbf259c53e2638d8d2b29164e87d7bb
Aniebiet Akpan <aniebiet.akpan@gmail.com>
2020-07-10 01:09:47 +0100

    Initialised spring boot project

---
+++ fitnessmonitor/src/main/java/com/project/fitnessmonitor/FitnessmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.project.fitnessmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessmonitorApplication.class, args);
+	}
+
+}
---
+++ fitnessmonitor/src/test/java/com/project/fitnessmonitor/FitnessmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fitnessmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
