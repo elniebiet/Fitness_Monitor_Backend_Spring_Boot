38c8dba6dc319f966ea87b5d021ebab1ebce6da0
Aniebiet Akpan <aniebiet.akpan@gmail.com>
2020-12-27 04:51:54 +0000

    update

--- fitnessmonitor/src/main/java/com/project/fitnessmonitor/FitnessmonitorApplication.java
+++ fitnessmonitor/src/main/java/com/project/fitnessmonitor/FitnessmonitorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitnessmonitorApplication {
 
 	public static void main(String[] args) {
+		/* start app */
 		SpringApplication.run(FitnessmonitorApplication.class, args);
 	}
 
